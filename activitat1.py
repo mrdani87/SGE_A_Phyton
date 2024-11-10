d0a48e25fea84f103dad2df9df2298177e33f467
mrdani87 <danial-javed@hotmail.com>
2024-11-10 17:52:27 +0100

    Primera activitat feta

---
+++ activitat1.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 15
+
+if num1 > num2:
+    resultat = f"El número {num2} és més gran que el número {num1}"
+elif num1 < num2:
+    resultat = f"El número {num1} és més petit que el número {num2}"
+else:
+    resultat = "Els dos números són iguals."
+
+print(resultat)
